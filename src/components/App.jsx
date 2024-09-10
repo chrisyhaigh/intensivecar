bd2a065b14cc713e312b721ebccab3521f350a18
Chrisy <chrisyhaigh@gmail.com>
2024-09-10 21:27:36 +0100

    Nav & App Component added

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../css/App.css';
+
+
+const App = () => {
+
+    return (
+        <div className="title">
+            <p>Intensive Car</p>
+        </div>
+    )
+}
+
+export default App
